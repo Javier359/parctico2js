68af40049c9904f48b9f507cd5613406c5488981
Javier Jimenez <javier.sniguel@gmail.com>
2023-11-05 23:27:55 -0300

    Se creo un archivo html para el ejercico y se lo resolvio

---
+++ js/ej9.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 500; i++) {
+    let mensaje = i;
+  
+    if (i % 4 === 0) {
+      mensaje += " (Múltiplo de 4)";
+    }
+  
+    if (i % 9 === 0) {
+      mensaje += " (Múltiplo de 9)";
+    }
+  
+    document.write(mensaje + "<br>");
+  
+    if (i % 5 === 0) {
+      document.write("<hr>");
+    }
+  }
+  
